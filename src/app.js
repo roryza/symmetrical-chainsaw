c44e2125770e1c6208dcb42aacc7a3214ce6ef23
Rory Austin <roryaustin@gmail.com>
2018-06-30 18:19:06 +0200

    Did some more styling

--- src/app.js
+++ src/app.js
@@ -28,3 +28,54 @@ window.populateCurrencies = (currencies, elementId) => {
 		elementRef.add(newSelect);
 	});
 }
+
+window.convert = () => {
+
+}
+
+
+window.registerServiceWorker = () => {
+	if (!navigator.serviceWorker) 
+	{
+		console.log('SW not supported');
+		return;
+	}
+
+	navigator.serviceWorker.register('/serviceworker.js').then(reg => {
+	  if (!navigator.serviceWorker.controller)
+		return;
+  
+	  if (reg.waiting) {
+		console.log('reg.waiting');
+		reg.waiting.postMessage({action: 'skipWaiting'});
+		return;
+	  }
+  
+	  if (reg.installing) {
+		console.log('reg.installing');
+		reg.installing.addEventListener('statechange', x => {
+			if (reg.installing.state == 'installed')
+				reg.installing.postMessage({action: 'skipWaiting'});
+		  });
+		return;
+	  }
+  
+	  reg.addEventListener('updatefound', x => {
+		console.log('updatefound');
+		reg.installing.addEventListener('statechange', x => {
+			if (reg.installing.state == 'installed')
+				reg.installing.postMessage({action: 'skipWaiting'});
+		  });
+	  });
+
+	});
+  
+	// Ensure refresh is only called once.
+	// This works around a bug in "force update on reload".
+	let refreshing = false;
+	navigator.serviceWorker.addEventListener('controllerchange', x => {
+	  if (refreshing) return;
+	  window.location.reload();
+	  refreshing = true;
+	});
+}
